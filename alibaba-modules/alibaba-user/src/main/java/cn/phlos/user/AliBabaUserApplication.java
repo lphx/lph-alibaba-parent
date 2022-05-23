c37ea919a38d81b5ad64cd078044fa8068723641
lph <1580780206@qq.com>
2022-05-23 23:03:03 +0800

    springcloud alibaba 创建工程注入到nacos

---
+++ alibaba-modules/alibaba-user/src/main/java/cn/phlos/user/AliBabaUserApplication.java
@@ -0,0 +1,19 @@
+package cn.phlos.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: AliBabaUserApplication
+ * @Author: lph
+ * @Description:
+ * @Date: 2022/5/22 22:13
+ */
+@SpringBootApplication
+public class AliBabaUserApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AliBabaUserApplication.class, args);
+    }
+}
